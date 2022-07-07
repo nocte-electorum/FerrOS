2b57ba7a8801daca74cf965ba2150d5a7acfd42d
Raforawesome <ubuntuhasnoroblox@gmail.com>
2022-07-07 17:12:50 -0400

    Added Color abstraction

---
+++ src/vga_buffer.rs
@@ -0,0 +1,20 @@
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u8)]
+pub enum Color {
+	Black = 0x0,
+	Blue = 0x1,
+	Green = 0x2,
+	Cyan = 0x3,
+	Red = 0x4,
+	Magenta = 0x5,
+	Brown = 0x6,
+	LightGray = 0x7,
+	DarkGray = 0x8,
+	LightBlue = 0x9,
+	LightGreen = 0xa,
+	LightCyan = 0xb,
+	LightRed = 0xc,
+	Pink = 0xd,
+	Yellow = 0xe,
+	White = 0xf,
+}
