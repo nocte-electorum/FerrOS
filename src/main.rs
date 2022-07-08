5e04e0227b2604f2102f6fbd3d6c07b327fa04f5
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2022-07-07 21:10:55 -0400

    Added VGABuffer test to main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,21 @@
 #![no_main]
 
 mod vga_buffer;
-use vga_buffer::Color;
+use vga_buffer::{ Color, VGABuffer };
 use core::panic::PanicInfo;
 
 const HELLO: &[u8] = b"Hello World!";
 // VGA Buffer address: 0xb8000
 #[no_mangle]
 pub unsafe extern "C" fn _start() -> ! {
-	let addr: *mut u8 = 0xb8000 as *mut u8;
+	// let addr: *mut u8 = 0xb8000 as *mut u8;
 
-	for (i, byte) in HELLO.iter().enumerate() {
-		*(addr.add(i * 2)) = *byte;
-		*(addr.add(i * 2 + 1)) = Color::Red as u8;
-	}
-
+	// for (i, byte) in HELLO.iter().enumerate() {
+	// 	*(addr.add(i * 2)) = *byte;
+	// 	*(addr.add(i * 2 + 1)) = Color::Red as u8;
+	// }
+	let mut buffer: VGABuffer = VGABuffer::new(Color::White);
+	buffer.write_str("Hello world 2!", None);
 	loop {}
 }
 
