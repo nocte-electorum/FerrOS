a8601611ca7529b72e3daff69e17c3928fad23b1
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2022-07-07 20:54:46 -0400

    Add `VGABuffer.write_char` and `VGABuffer.write_str` methods

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -88,4 +88,14 @@ impl VGABuffer {
 			self.writer.write_byte(color, *byte);
 		}
 	}
+
+	fn write_char(&mut self, c: char, color_override: Option<Color>) {
+		let color: Color = color_override.unwrap_or(self.color);
+		self.writer.write_byte(color, c as u8);
+	}
+
+	/// Convenience function
+	fn write_str(&mut self, to_write: &str, color_override: Option<Color>) {
+		self.write_bytes(to_write.as_bytes(), color_override);
+	}
 }
